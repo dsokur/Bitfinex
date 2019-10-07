d9d0eee2de9f634cfdeaed4674624e5ec31aced1
Oleg Gavrilin <oleg.gavrilin@uptech.team>
2019-10-07 17:46:53 +0300

    add ping

--- src/containers/Ticker.js
+++ src/containers/Ticker.js
@@ -1,33 +1,42 @@
 import {connect} from 'react-redux'
 import Ticker from '../components/Ticker/Ticker';
 import React, {Component} from 'react';
-import {wsSubscribe, wsUnsubscribe} from "../services/webSockets";
+import {wsInit, wsSubscribe, wsUnsubscribe} from "../services/webSockets";
 import {onTickerRequestData, onTickerSubscribe, onTickerUnsubscribe} from "../reducers/Ticker";
+import {chanelTypes} from "../constants";
 
 class TickerContainer extends Component {
-	componentDidMount() {
-		wsSubscribe('ticker', this.props.onTickerSubscribe, this.props.onTickerRequestData)
-	}
+    constructor(props) {
+        super(props);
+        wsInit(chanelTypes.TICKER)
+    }
 
-	componentWillUnmount() {
-		wsUnsubscribe('ticker', this.props.chanId, this.props.onTickerUnsubscribe)
-	}
+    componentDidMount() {
+        wsSubscribe(chanelTypes.TICKER, this.props.onTickerSubscribe, this.props.onTickerRequestData)
+    }
 
-	render() {
-		return <Ticker {...this.props}/>
-	}
+    componentWillUnmount() {
+        wsUnsubscribe(chanelTypes.TICKER, this.props.chanId, this.props.onTickerUnsubscribe)
+    }
+
+    render() {
+        return <Ticker {...this.props}/>
+    }
 }
 
-const mapStateToProps = ({Ticker: {
-	chanId, LAST_PRICE,VOLUME,DAILY_CHANGE,
-	DAILY_CHANGE_PERC,
-	LOW,
-	HIGH}}) => {
+const mapStateToProps = ({
+                             Ticker: {
+                                 chanId, LAST_PRICE, VOLUME, DAILY_CHANGE,
+                                 DAILY_CHANGE_PERC,
+                                 LOW,
+                                 HIGH
+                             }
+                         }) => {
     return {
-	    chanId, LAST_PRICE,VOLUME,DAILY_CHANGE,
-	    DAILY_CHANGE_PERC,
-	    LOW,
-	    HIGH
+        chanId, LAST_PRICE, VOLUME, DAILY_CHANGE,
+        DAILY_CHANGE_PERC,
+        LOW,
+        HIGH
     };
 };
 
--- src/services/webSockets.js
+++ src/services/webSockets.js
@@ -2,29 +2,48 @@ import config from '../configs'
 
 const connections = {}
 
-export const wsSubscribe = (type, onSubscribe, onRequest) => {
+export const wsInit = (chanelType) => {
     const wss = new WebSocket(config.WS_PUBLIC_API);
-    connections[type] = wss
+    connections[chanelType] = wss;
+    wss.onopen = () => {
+        setInterval(() => ping(chanelType), 1000);
+    };
+}
+
+export const ping = (chanelType) =>{
+    const wss = connections[chanelType];
     const msg = JSON.stringify({
-        event: 'subscribe',
-        channel: type,
-        symbol: 'tBTCUSD'
+        "event":"ping",
+        "cid": 1234
     });
     wss.onopen = () => {
-        wss.send(msg);
-    };
+        wss.send(msg)
+    }
+};
+
+export const wsSubscribe = (chanelType, onSubscribe, onRequest) => {
+    const wss = connections[chanelType];
+    const msg = JSON.stringify({
+        event: 'subscribe',
+        channel: chanelType,
+        symbol: 'tBTCUSD'
+    });
+    wss.onopen = () => { wss.send(msg)};
+
     wss.onmessage = (e) => {
         onMessage(onSubscribe, onRequest, e)
     };
 };
 
-export const wsUnsubscribe = (type, chanId, onUnsubscribe) => {
-    const wss = connections[type];
+export const wsUnsubscribe = (chanelType, chanId, onUnsubscribe) => {
+    const wss = connections[chanelType];
     const msg = JSON.stringify({
         "event": "unsubscribe",
         chanId: chanId,
     });
-    wss.send(msg);
+    wss.onopen = () => {
+        wss.send(msg)
+    };
     wss.onmessage = (event) => {
         const response = JSON.parse(event.data);
         console.log(response)
