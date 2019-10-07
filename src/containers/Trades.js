be60845f2b52a3c54af46c7e03d1446a769dbc7b
Dmytro <dmytro.sokur@gmail.com>
2019-10-07 17:38:24 +0300

    Trades reducer/container

--- src/containers/Trades.js
+++ src/containers/Trades.js
@@ -1,22 +1,44 @@
 import {connect} from 'react-redux'
 import Trades from '../components/Trades/Trades';
-import {wsSubscribe} from "../services/webSockets";
+import {wsSubscribe, wsUnsubscribe} from "../services/webSockets";
+import {onTradesRequestData, onTradesSubscribe, onTradesUnsubscribe} from "../reducers/Trades";
 import React, {Component} from 'react';
 
 class TradesContainer extends Component {
-    componentDidMount(){
-    }
-    render() {
-        return (
-            <Trades/>
-        );
-    }
+	componentDidMount() {
+		wsSubscribe('trade', this.props.onTradesSubscribe, this.props.onTradesRequestData)
+	}
+
+	componentWillUnmount() {
+		wsUnsubscribe('trade', this.props.chanId, this.props.onTradesUnsubscribe)
+	}
+
+	render() {
+		console.log('Trade', this.props)
+		return <Trades {...this.props}/>
+	}
 }
 
-const mapStateToProps = (state) => {
-    return {
-        state
-    };
+const mapStateToProps = ({
+	                         Trades: {
+		                         chanId,
+		                         ID,
+		                         MTS,
+		                         AMOUNT,
+		                         PRICE,
+		                         RATE,
+		                         PERIOD
+	                         }
+                         }) => {
+	return {
+		chanId,
+		ID,
+		MTS,
+		AMOUNT,
+		PRICE,
+		RATE,
+		PERIOD
+	};
 };
 
-export default connect(mapStateToProps,)(TradesContainer);
+export default connect(mapStateToProps, {onTradesRequestData, onTradesSubscribe, onTradesUnsubscribe})(TradesContainer);
--- src/reducers/Trades.js
+++ src/reducers/Trades.js
@@ -1,12 +1,58 @@
+const TRADES_SUBSCRIBED = 'TRADES_SUBSCRIBED';
+const TRADES_UNSUBSCRIBED = 'TRADES_UNSUBSCRIBED';
+const REQUEST_TRADES_DATA = 'REQUEST_TRADES_DATA';
 
 const initialState = {
-	Trades: '',
+	chanId: null,
+	ID: null,
+	MTS: null,
+	AMOUNT: null,
+	PRICE: null,
+	RATE: null,
+	PERIOD: null,
 };
 
 export default (state = initialState, action) => {
 	const {payload} = action;
+
 	switch (action.type) {
+		case(TRADES_SUBSCRIBED): {
+			return {...state, chanId: payload.chanId}
+		}
+		case(TRADES_UNSUBSCRIBED): {
+			return {...initialState}
+		}
+		case(REQUEST_TRADES_DATA): {
+			const [ID,
+				MTS,
+				AMOUNT,
+				PRICE,
+				RATE,
+				PERIOD] = action.payload;
+			return {
+				...state, ID,
+				MTS,
+				AMOUNT,
+				PRICE,
+				RATE,
+				PERIOD
+			}
+		}
 		default:
 			return state;
 	}
 };
+
+export const onTradesSubscribe = (data) => ({
+	type: TRADES_SUBSCRIBED,
+	payload: data
+})
+
+export const onTradesUnsubscribe = () => ({
+	type: TRADES_UNSUBSCRIBED,
+})
+
+export const onTradesRequestData = (data) => ({
+	type: REQUEST_TRADES_DATA,
+	payload: data
+})
